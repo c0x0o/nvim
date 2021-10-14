566806599a41757b369341d80b6e02e91047359e
lyzongyuan <lyzongyuan@gmail.com>
2021-10-14 11:41:06 +0800

    fix: keep up-to-date with nvim-tree

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -76,7 +76,15 @@ local function setup()
 		auto_close = false, -- 0 by default, closes the tree when it's the last window
 		open_on_tab = false, -- 0 by default, will open the tree when entering a new tab and the tree was previously open
 		hijack_cursor = false, -- 1 by default, when moving cursor in the tree, will position the cursor at the start of the file on the current line
-		lsp_diagnostics = true, -- 0 by default, will show lsp diagnostics in the signcolumn. See :help nvim_tree_lsp_diagnostics
+		diagnostics = {
+			enable = true,
+			icons = {
+				hint = '',
+				info = '',
+				warning = '',
+				error = ''
+			}
+		},
 		update_cwd = false, -- 1 by default, will update the tree cwd when changing nvim's directory (DirChanged event). Behaves strangely with autochdir set.
 		-- hijacks new directory buffers when they are opened.
 		update_to_buf_dir = {

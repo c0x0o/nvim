f20f06b0d46ec5d130d6d2a02bbc6f13b0040672
lyzongyuan <lyzongyuan@gmail.com>
2021-12-01 14:35:34 +0800

    fix: keep nvim-tree setting up-to-date

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -1,5 +1,4 @@
 local function setup()
-	vim.g.nvim_tree_gitignore = 0 -- 0 by default
 	vim.g.nvim_tree_quit_on_open = 0 -- 0 by default, closes the tree when you open a file
 	vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
 	vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
@@ -105,7 +104,10 @@ local function setup()
 		filters = {
 			dotfiles = false,
 			custom = { '.git', 'node_modules', '.cache' }
-		}
+		},
+		git = {
+			ignore = false,
+		},
 	}
 
 	vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<CR>', { noremap = true, silent = true})

de946f5eb97a464b7a9f1120528fe3d0362fec0e
lyzongyuan <lyzongyuan@gmail.com>
2022-01-10 10:16:44 +0800

    fix: git-blamer looks better and wont appear in nvim-tree

--- lua/config/plugins/git-blame.lua
+++ lua/config/plugins/git-blame.lua
@@ -1,6 +1,17 @@
 local function setup()
 	vim.g.gitblame_message_template = '   <author>, <date> • <summary> • <sha>'
 	vim.g.gitblame_date_format = '%r'
+	vim.g.gitblame_highlight_group = 'Blamer'
+
+	-- do not show blamer in some situation
+	vim.cmd([[
+	augroup disableblamer
+		autocmd!
+		autocmd disableblamer WinEnter NvimTree GitBlameDisable 
+		autocmd disableblamer BufEnter NvimTree GitBlameDisable 
+		autocmd disableblamer BufLeave NvimTree GitBlameEnable
+	augroup END
+	]])
 end
 
 return {

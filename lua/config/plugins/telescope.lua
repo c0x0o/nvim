74e9ab59e7098e6cbb7173938e9175ea91050da0
Zongyuan Li <zongyuan.li@smartx.com>
2024-06-04 14:16:40 +0800

    improve terminal && telescope search

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,6 +1,6 @@
 local function setup()
     vim.api.nvim_set_keymap('n', '<C-p>', "<cmd>lua require('telescope.builtin').find_files()<cr>", {})
-    vim.api.nvim_set_keymap('n', '<C-f>', "<cmd>lua require('telescope.builtin').live_grep()<cr>", {})
+    vim.api.nvim_set_keymap('n', '<C-f>', "<cmd>lua require('telescope.builtin').live_grep{only_sort_text=true}<cr>", {})
 end
 
 return {
--- lua/config/plugins/terminal.lua
+++ lua/config/plugins/terminal.lua
@@ -1,4 +1,11 @@
 require("toggleterm").setup {
+    size = function(term)
+        if term.direction == "horizontal" then
+            return vim.o.lines * 0.33
+        elseif term.direction == "vertical" then
+            return vim.o.columns * 0.33
+        end
+    end,
     start_in_insert = true,
 }
 
